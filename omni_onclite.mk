3538934d370e4567903db269f2affb14f9db50eb
kostyajrzaws <kostyajrzaws@gmail.com>
2019-09-20 08:44:11 +0000

    change omni_onclite.mk
    Change-Id: I6fc960b548bc2d9d80f9f5ec626af34f5723b68c

--- omni_onclite.mk
+++ omni_onclite.mk
@@ -4,6 +4,7 @@ $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := onclite
+PRODUCT_DEVICE := onc
 PRODUCT_NAME := omni_onclite
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Xiaomi Redmi 7
@@ -18,3 +19,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.usb.rmnet.func.name=rmnet_bam
 
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=onclite PRODUCT_NAME=onclite
+
