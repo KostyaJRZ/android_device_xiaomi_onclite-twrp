a04260aa5d45e3343ca429d9824d75db82ee1c72
Galitsky Dmytro <melles.android@gmail.com>
2019-10-03 13:39:10 +0300

    REVERT : fix ro.product

--- omni_onclite.mk
+++ omni_onclite.mk
@@ -24,6 +24,7 @@ PRODUCT_DEVICE := onclite
 PRODUCT_NAME := omni_onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi Redmi 7
+<<<<<<< HEAD
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
@@ -33,3 +34,16 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
+=======
+PRODUCT_MANUFACTURER := xiaomi
+TARGET_VENDOR_PRODUCT_NAME := onclite
+TARGET_VENDOR_DEVICE_NAME := onclite
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=sdm632 \
+    sys.usb.controller=7000000.dwc3 \
+    sys.usb.rndis.func.name=rndis_bam \
+    sys.usb.rmnet.func.name=rmnet_bam
+
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=onclite PRODUCT_NAME=onclite
+>>>>>>> parent of 82623a1... fix ro.product
