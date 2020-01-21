32d86cbb2c51af0bf953cba060ac721b5ed75fe9
Galitsky Dmytro <melles.android@gmail.com>
2020-01-21 09:33:20 +0200

    data

--- omni_onclite.mk
+++ omni_onclite.mk
@@ -32,4 +32,6 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.build.product
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
+    ro.vendor.build.security_patch=2099-12-31 \
+	ro.secure=1 \
+	ro.adb.secure=0
